1db3cf6d72ed523946edb18dc04345742a02be80
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-13 12:16:57 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -10,11 +10,11 @@ using namespace std;
 void itc_even_index_list (const vector <int> &a1, vector <int> &a2);
 void itc_even_parts_list(const vector <int> &a1, vector <int> &a2);
 int itc_positive_list(const vector <int> &a);
-int itc_sl_list(const vector <int> &mass);
-bool itc_same_parts_list(const vector <int> &mass);
-void itc_rev_list(vector <int> &mass);
-void itc_rev_par_list(vector <int> &mass);
-void itc_rshift_list(vector <int> &mass);
-void itc_super_shift_list(vector <int> &mass, int n);
+int itc_sl_list(const vector <int> &a);
+bool itc_same_parts_list(const vector <int> &a);
+void itc_rev_list(vector <int> &a);
+void itc_rev_par_list(vector <int> &a);
+void itc_rshift_list(vector <int> &a);
+void itc_super_shift_list(vector <int> &a, int n);
 
 #endif // EASY_LIST_H_INCLUDED
