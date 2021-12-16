bb34568d8b76809b570bc10fd262182a5b49600c
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-16 17:26:11 +0300

    Update itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++ itc_rshift_list.cpp
@@ -1,9 +1,11 @@
 #include "easy_list.h"
 
-void itc_rshift_list(vector <int> &a){
-    int tmp = a[a.size()-1];
-    for(int i = a.size() - 1; i > 0; i--){
-        a[i] = a[i - 1];
+void itc_rshift_list(vector <int> &a) {
+    if (a.size() != 0){
+        vector <int> a2(a.size());
+        a2[0] = a[a.size() - 1];
+        for (int i = 1; i < a.size(); i++)
+            a2[i] = a[i - 1];
+        a = a2;
     }
-    a[0] = tmp;
 }
