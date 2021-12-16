88c7d0a339fbe47fc9a7e2eeb03d3c219f65c562
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-16 17:33:38 +0300

    Update itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++ itc_rshift_list.cpp
@@ -9,3 +9,15 @@ void itc_rshift_list(vector <int> &a) {
         a = a2;
     }
 }
+
+void itc_lshift(vector <int> &a){
+    int counter = a.size();
+    vector<int> a2 = a;
+    if(counter != 0){
+        for(int i = counter - 2; i >= 0; i = i - 2){
+            a[i] = a2[i + 1];
+            a[i - 1] = a2[i];
+        }
+        a[counter - 1] = a2[0];
+    }
+}
