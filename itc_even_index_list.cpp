a794ae948cbd62e331665027e1282a56cd984002
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-16 14:01:39 +0300

    Update itc_even_index_list.cpp

--- itc_even_index_list.cpp
+++ itc_even_index_list.cpp
@@ -1,6 +1,6 @@
 #include "easy_list.h"
-void itc_even_index_list (const vector <int> &a1, vector <int> &a2){
-    for(int i = 0; i < a1.size(); i+=2){
-        a2.push_back(a1[i]);
-    }
+void itc_even_index_list(const vector <int> &a1, vector <int> &a2) {
+    if (a1.size() != 0)
+        for (int i = 0; i < a1.size(); i += 2)
+            a2.push_back(a1[i]);
 }
