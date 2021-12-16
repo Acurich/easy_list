cdc7009e11132ee3c0177441c3075a01190b2898
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-16 17:24:42 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -1,10 +1,12 @@
 #include "easy_list.h"
 
-void itc_rev_par_list(vector <int> &a){
-    int tmp;
-    for(int i = 0; i < a.size() - 1; i+=2){
-        tmp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = tmp;
+void itc_rev_par_list(vector <int> &a) {
+    if (a.size() != 0){
+        int tmp;
+        for (int i = 1; i < a.size(); i += 2){
+            tmp = a[i];
+            a[i] = a[i - 1];
+            a[i - 1] = tmp;
+    }
     }
 }
