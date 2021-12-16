5d68491553a764b2b0ab3e0dd3bd93fe56ec7114
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-16 17:18:35 +0300

    Update itc_even_parts_list.cpp

--- itc_even_parts_list.cpp
+++ itc_even_parts_list.cpp
@@ -1,8 +1,7 @@
 #include "easy_list.h"
-void itc_even_parts_list(const vector <int> &a1, vector <int> &a2){
-    for(int i = 0; i < a1.size(); i++){
-        if(a1[i] % 2 == 0){
-            a2.push_back(a1[i]);
-        }
-    }
+void itc_even_parts_list(const vector<int> &a1, vector<int> &a2) {
+    if (a1.size() != 0)
+        for(int i = 0; i < a1.size(); i++)
+            if(a1[i] % 2 == 0)
+                a2.push_back(a1[i]);
 }
