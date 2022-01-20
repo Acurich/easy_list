3071eca11d53a842752310deca3c8c22dce3bc0c
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-21 00:06:28 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -1,13 +1,6 @@
 #include "easy_list.h"
 int itc_positive_list(const vector <int> &a){
-    int counter = a.size();
-    int Counter = 0;
-    if(counter != 0){
-        for(int i = 0; i < counter; i++){
-            if(a[i] >= 0){
-                Counter++;
-            }
-        }
-    }
+    int counter = a.size(), Counter = 0;
+    if(counter != 0){ for(int i = 0; i < counter; i++){ if(a[i] >= 0){ Counter++;}}}
     return Counter;
 }
