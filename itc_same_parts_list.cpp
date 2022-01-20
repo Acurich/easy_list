865e878a9ae35f7c8386bbc296ed461db7755d0d
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-21 00:10:52 +0300

    Update itc_same_parts_list.cpp

--- itc_same_parts_list.cpp
+++ itc_same_parts_list.cpp
@@ -1,20 +1,8 @@
 #include "easy_list.h"
 
 bool itc_same_parts_list(const vector <int> &a){
-    int counter = a.size();
-    int c = 0;
-    if(counter != 0){
-        for(int i = 0; i < counter; i++){
-            if(a[i] < 0 and a[i + 1] < 0){
-                c++;
-            }
-            if(a[i] >= 0 and a[i + 1] >= 0 and i + 1 < counter){
-                c++;
-            }
-        }
-    }
-    if(c > 0){
-        return true;
-    }
+    int counter = a.size(), c = 0;
+    if(counter != 0){for(int i = 0; i < counter; i++){ if(a[i] < 0 and a[i + 1] < 0){ c++;} if(a[i] >= 0 and a[i + 1] >= 0 and i + 1 < counter){c++;}}}
+    if(c > 0){return true;}
     return false;
 }
