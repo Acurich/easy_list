18a9adccd15c89fd368363cbab5fe2ab68e8de72
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-21 00:09:06 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -1,12 +1,5 @@
 #include "easy_list.h"
 
 void itc_rev_par_list(vector <int> &a) {
-    if (a.size() != 0){
-        int tmp;
-        for (int i = 1; i < a.size(); i += 2){
-            tmp = a[i];
-            a[i] = a[i - 1];
-            a[i - 1] = tmp;
-    }
-    }
+    if (a.size() != 0){ int tmp; for (int i = 1; i < a.size(); i += 2){ tmp = a[i]; a[i] = a[i - 1]; a[i - 1] = tmp;}}
 }
