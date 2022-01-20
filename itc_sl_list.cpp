1d810655ce3301fefb5c68305cfad8f81069d9f4
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-21 00:12:27 +0300

    Update itc_sl_list.cpp

--- itc_sl_list.cpp
+++ itc_sl_list.cpp
@@ -1,13 +1,5 @@
 #include "easy_list.h"
 
 int itc_sl_list(const vector <int> &a) {
-    if (a.size() != 0){
-        int counter = 0;
-        for (int i = 1; i < a.size(); i++){
-            if (a[i] > a[i - 1]){
-                counter += 1;
-            }
-        }
-        return counter;
-    }
+    if (a.size() != 0){ int counter = 0; for (int i = 1; i < a.size(); i++){ if (a[i] > a[i - 1]){ counter += 1;}}return counter;}
 }
