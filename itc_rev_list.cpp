862e13dc902c74a8175a1755b66c2a65af61dd6b
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-21 00:07:54 +0300

    Update itc_rev_list.cpp

--- itc_rev_list.cpp
+++ itc_rev_list.cpp
@@ -1,12 +1,5 @@
 #include "easy_list.h"
 
 void itc_rev_list(vector <int> &a) {
-    if (a.size() != 0){
-        int tmp;
-        for (int i = 0; i < a.size() / 2; i++){
-            tmp = a[i];
-            a[i] = a[a.size() - i - 1];
-            a[a.size() - i - 1] = tmp;
-        }
-    }
+    if (a.size() != 0){ int tmp; for (int i = 0; i < a.size() / 2; i++){ tmp = a[i]; a[i] = a[a.size() - i - 1]; a[a.size() - i - 1] = tmp;}}
 }
